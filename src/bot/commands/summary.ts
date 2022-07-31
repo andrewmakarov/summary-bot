2ea20563d35766d7380564bafc4df87fefa54a3f
Andrew Makarov <andrewmakarov@inbox.ru>
2022-07-31 22:40:25 +0400

    Support summary comand

--- src/bot/commands/summary.ts
+++ src/bot/commands/summary.ts
@@ -1,14 +1,19 @@
 import { Context } from 'telegraf';
-import { createDocumentsLayout } from '../../buttonLayout';
 import { SheetModel } from '../../model/sheetModel';
-// import { UserModel } from '../../model/userModel';
+import { UserModel } from '../../model/userModel';
+import SheetsEditor from '../../sheetsEditor';
+import { getSummaryText, iCountText } from '../../textUtils';
 import { createCommand } from './base/createCommand';
 
-const command = (ctx: Context, sheetModel: SheetModel) => {
-    ctx.reply('Установи текущий документ', {
-        reply_markup: {
-            inline_keyboard: createDocumentsLayout(),
-        },
+const command = async (ctx: Context, sheetModel: SheetModel, userModel: UserModel, sheetEditor: SheetsEditor) => {
+    const t = await ctx.reply(iCountText);
+
+    const user = await userModel.getUser(ctx.from!.id);
+    const document = sheetModel.documents.find((d) => d.id === user!.currentDocumentId);
+    const value = await sheetEditor.getSummary(document?.id!);
+
+    ctx.telegram.editMessageText(t.chat.id, t.message_id, undefined, getSummaryText(value), {
+        parse_mode: 'Markdown',
     });
 };
 
