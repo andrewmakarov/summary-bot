70ee925ead8737e5bd2367847d9125edee5b918d
Andrew Makarov <andrewmakarov@inbox.ru>
2022-10-30 22:36:35 +0400

    Refactor factory

--- src/intermediateTypes.ts
+++ src/intermediateTypes.ts
@@ -1,4 +1,4 @@
 import { ICacheItemBody, WithKey } from './cache';
 import { IPureUser } from './model/userModel';
 
-export type AmountInfo = WithKey<ICacheItemBody> & { user?: IPureUser, categoryIndex: number };
+export type AmountInfo = WithKey<ICacheItemBody> & { user?: IPureUser, categoryIndex: number }; // TODO move to cache
